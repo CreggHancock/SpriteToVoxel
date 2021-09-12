145d30a5911a10e422fe9bf6d489867efa880d48
SLGamesCregg <theheartlandsaga@gmail.com>
2021-09-12 16:47:06 -0500

    adding comments and cleaning up syntax

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-
+//reset state on load/reload
 function onLoad() {
 	document.getElementById('convert').disabled = true;
 	document.getElementById('save').disabled = true;
@@ -7,18 +7,20 @@ function onLoad() {
 	document.getElementById('settings-popup').style.visibility = 'hidden';
 }
 
+//open an image file and preview it on a canvas
 function openFile(file) {
-	var input = file.target;
+	let input = file.target;
 	output = document.getElementById('output');
-	var preview = document.getElementById('imagepreview');
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
-	var ctx = preview.getContext('2d');
+	let preview = document.getElementById('imagepreview');
+	let canvas = document.getElementById('canvas');
+	let context = canvas.getContext('2d');
+	let ctx = preview.getContext('2d');
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.clearRect(0, 0, preview.width, preview.height);
-    var reader = new FileReader();
+    let reader = new FileReader();
     reader.onload = function(){
-		var dataURL = reader.result;
+		//reset state from previously loaded image
+		let dataURL = reader.result;
 		output.src = dataURL;
 		objNormals = [];
 		objVertices = [];
@@ -27,7 +29,6 @@ function openFile(file) {
 	};
 	let nameArr = input.files[0].name.split(".");
 	fileName = nameArr[0];
-	console.log("input " + fileName);
 	reader.readAsDataURL(input.files[0]);
 	output.onload = function(){
 		canvasPosX = 0;
@@ -43,37 +44,40 @@ function openFile(file) {
 	}
   }
 
+//read data from loaded image, and create vert, normal, and face data to cache for each pixel
 function convertToObj() {
-	var canvas = document.getElementById('canvas');
+	let canvas = document.getElementById('canvas');
 	canvas.width = canvasWidth;
 	canvas.height = canvasHeight;
-	var context = canvas.getContext('2d');
+	let context = canvas.getContext('2d');
 	context.transform(1, 0, 0, -1, 0, canvas.height)
 	context.drawImage(output, canvasPosX, canvasPosY);
 	document.getElementById('convert').disabled = true;
 	document.getElementById('save').disabled = false;
 	document.getElementById('save-label').innerHTML = "Conversion Successful";
-	for (var x=0;x<output.width;x++)
+	for (let x=0;x<output.width;x++)
 	{
-		for (var y=0;y<output.height;y++)
+		for (let y=0;y<output.height;y++)
 		{
-			var data = context.getImageData(x, y, 1, 1).data;
+			let data = context.getImageData(x, y, 1, 1).data;
 			if (data[3] == 0)
 			{
 				continue;
 			}
-			var rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
 			createCubeAtPosition(x, y, data)
 		}
 	}
 }
 
+
+//when called from the save button, convert cached data into obj and mtl text files and save them in a zip
 function saveModel() {
 	let matBlob = saveMtl();
 	let objBlob = saveObj();
 	downloadObjZip(matBlob, objBlob);
 }
 
+//download zipped obj and mtl for new model
 async function downloadObjZip(mtlContent, objContent) {
 	// define what we want in the ZIP
 	const obj = { name: fileName + '.obj', lastModified: new Date(), input: objContent };
@@ -92,6 +96,7 @@ async function downloadObjZip(mtlContent, objContent) {
 	// in real life, don't forget to revoke your Blob URLs if you use them
   }
 
+//get string data for obj
 function saveObj() {
 	let objString = getObjString();
 
@@ -99,6 +104,7 @@ function saveObj() {
 	return stringToBlob;
 }
 
+//get string data for mtl
 function saveMtl() {
 	let mtlString = getMtlString();
 
@@ -106,6 +112,7 @@ function saveMtl() {
 	return stringToBlob;
 }
 
+//create vert, face, and mtl data for a pixel at a position of the image
 function createCubeAtPosition(x, y, rgba) {
 	let posOffset = objVertices.length;
 	objVertices.push([x, y + 1, 1]);
@@ -127,8 +134,9 @@ function createCubeAtPosition(x, y, rgba) {
 
 }
 
+//converts cached data into a string that can be saved as a .obj
 function getObjString() {
-	var objString = "# cube.obj\r\n#\r\n\r\nmtllib " + fileName + ".mtl\r\n\r\ng cube\r\n\r\n";
+	let objString = "# cube.obj\r\n#\r\n\r\nmtllib " + fileName + ".mtl\r\n\r\ng cube\r\n\r\n";
 	for (let v=0;v<objVertices.length;v++) {
 		let vert = objVertices[v];
 		objString += `v ${vert[0]} ${vert[1]} ${vert[2]}\r\n`;
@@ -138,7 +146,7 @@ function getObjString() {
 		objString += `g ${objMtl[m][1]}\r\nusemtl ${objMtl[m][1]}\r\n`;
 		for (let p=0;p<objMtl[m][2].length;p++)
 		{
-			var offset = objMtl[m][2][p];
+			let offset = objMtl[m][2][p];
 			for (let side=0;side<6;side++)
 			{
 				let face = objFaces[offset+side];
@@ -149,9 +157,11 @@ function getObjString() {
 	return objString;
 }
 
+
+//converts cached mtl data into a string that can be saved as a .mtl material library
 function getMtlString() {
-	var mtlString = "";
-	for (var m=0; m<objMtl.length; m++) {
+	let mtlString = "";
+	for (let m=0; m<objMtl.length; m++) {
 		mtlString += `newmtl ${objMtl[m][1]}\r\n`;
 		mtlString += 'illum 1\r\n';
 		mtlString += `Ka ${objMtl[m][0][0] / 255} ${objMtl[m][0][1] / 255} ${objMtl[m][0][2] / 255}\r\n`
@@ -160,9 +170,10 @@ function getMtlString() {
 	return mtlString;
 }
 
+//check if we have a material for this color yet, and if not create one with a generated name
 function findOrCreateMtl(rgba, posOffset)
 {
-	var mtlName = null;
+	let mtlName = null;
 	for (var i=0;i<objMtl.length;i++) {
 		if (objMtl[i][0][0] == rgba[0] && objMtl[i][0][1] == rgba[1] && objMtl[i][0][2] == rgba[2]) {
 			objMtl[i][2].push(posOffset);
@@ -170,31 +181,35 @@ function findOrCreateMtl(rgba, posOffset)
 		}
 	}
 	if (mtlName == null) {
+		//generates a name. currently the name can be pretty limited
+		//TODO: refactor name generator to allow for any amount of colors
 		mtlName = "mat";
-		var nIndex = objMtl.length;
-		var firstIndex = Math.floor(nIndex / mtlNames.length);
-		var secondIndex = nIndex % mtlNames.length;
-		var offsetArr = [posOffset];
+		let nIndex = objMtl.length;
+		let firstIndex = Math.floor(nIndex / mtlNames.length);
+		let secondIndex = nIndex % mtlNames.length;
+		let offsetArr = [posOffset];
 		mtlName += mtlNames[firstIndex];
 		mtlName += mtlNames[secondIndex];
 		objMtl.push([rgba, mtlName, offsetArr]);
 	}
 }
 
+//toggle visibility of settings popup
 function toggleSettings() {
-	var settings = document.getElementById('settings-popup');
+	let settings = document.getElementById('settings-popup');
 	settings.style.visibility = settings.style.visibility == 'hidden' ? 'visible' : 'hidden';
 }
 
+//wip system for setting a partial rect of an image to be used instead of whole image for generating model
 function updateRect() {
 	canvasPosX = parseInt(document.getElementById('x-setter').value);
 	canvasPosY = parseInt(document.getElementById('y-setter').value);
 	canvasWidth = parseInt(document.getElementById('width').value);
 	canvasHeight = parseInt(document.getElementById('height').value);
-	var preview = document.getElementById('imagepreview');
-	var canvas = document.getElementById('canvas');
-	var context = canvas.getContext('2d');
-	var ctx = preview.getContext('2d');
+	let preview = document.getElementById('imagepreview');
+	let canvas = document.getElementById('canvas');
+	let context = canvas.getContext('2d');
+	let ctx = preview.getContext('2d');
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.clearRect(0, 0, preview.width, preview.height);
 	preview.height = canvasHeight;
@@ -205,18 +220,26 @@ function updateRect() {
 	context.drawImage(output, canvasPosX, canvasPosY)
 }
 
+//update preview canvas background color based off settings. useful if you need contrast from your sprite when previewing, but has no effect on the model
 function updateBackground(colorPicker) {
-	var canvas = document.getElementById('imagepreview');
-	var color = colorPicker.target.value;
+	let canvas = document.getElementById('imagepreview');
+	let color = colorPicker.target.value;
 	canvas.style.backgroundColor = color;
 }
 
+//an array of all vertices in the mesh represented with nested arrays of [x, y, z]
 let objVertices = [];
+//an array of nested arrays that represent various parts of a matial, represented by [color, material name, [cubes that use material]]
 let objMtl = [];
+//an array of the faces of the material, represented by nested arrays of each vertice (corner) that makes up the face, [vert1, vert2, vert3, vert4]
 let objFaces = [];
-let mtlNames = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//temporarily used for generating material names, will need to be refactored
+const mtlNames = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//img that normally is hidden
 let output = null;
+//name of .obj and .mtl file to save as, default set to name of image uploaded
 let fileName = "";
+//caching for wip setting for region/rect selection for conversion
 let canvasPosX = 0;
 let canvasPosY = 0;
 let canvasWidth = 0;
